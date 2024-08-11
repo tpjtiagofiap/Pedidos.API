dd1929fa9fa4189b3f6be2eb2100690570abc8a8
Tiago Pedro Jr <tpj.tiago@gmail.com>
2024-08-10 21:52:23 -0300

    Add project files.

---
+++ Pedidos.API/Pedidos.cs
@@ -0,0 +1,9 @@
+﻿namespace Pedidos.API
+{
+    public class Pedido
+    {
+        public int Numero { get; set; }
+        public string Produto { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
